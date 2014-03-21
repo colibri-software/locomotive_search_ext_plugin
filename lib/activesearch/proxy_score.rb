41f615278c33e203f09264ef5c2d21f4c067a608
Charlie Greene <greeneca@gmail.com>
2014-03-21 13:49:53 -0300

    fix proxy score patch

--- lib/activesearch/proxy_score.rb
+++ lib/activesearch/proxy_score.rb
@@ -2,9 +2,19 @@ require "activesearch/proxy"
 
 module ActiveSearch
   class Proxy
+    include Enumerable
+
+    def initialize(text, conditions, options = {}, &implementation)
+      @text           = text
+      @conditions     = conditions
+      @options        = options
+      @implementation = implementation
+    end
 
     def each(&block)
-      @implementation.call(@text, @conditions).each { |result| block.call(Result.new(result), generate_score(result))  }
+      @implementation.call(@text, @conditions).each do |result|
+        block.call(Result.new(result, @text, @options), generate_score(result))
+      end
     end
 
     protected
